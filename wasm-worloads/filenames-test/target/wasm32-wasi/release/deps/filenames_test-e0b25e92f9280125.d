7f7a2fb84f7cd22b895b37828f3a6dd28b5e01a2
afrpauwe <arthur.pauwels@ugent.be>
2024-08-06 10:19:21 +0200

    clean up

--- wasm-worloads/filenames-test/target/wasm32-wasi/release/deps/filenames_test-e0b25e92f9280125.d
+++
@@ -1,5 +0,0 @@
-/Users/arthurpauwels/Documents/Masterproef/mp-k8s/wasm-worloads/filenames-test/target/wasm32-wasi/release/deps/filenames_test-e0b25e92f9280125.wasm: src/main.rs
-
-/Users/arthurpauwels/Documents/Masterproef/mp-k8s/wasm-worloads/filenames-test/target/wasm32-wasi/release/deps/filenames_test-e0b25e92f9280125.d: src/main.rs
-
-src/main.rs:
--- wasm-worloads/filenames-test/target/wasm32-wasi/release/filenames-test.d
+++
@@ -1,1 +0,0 @@
-/Users/arthurpauwels/Documents/Masterproef/mp-k8s/wasm-worloads/filenames-test/target/wasm32-wasi/release/filenames-test.wasm: /Users/arthurpauwels/Documents/Masterproef/mp-k8s/wasm-worloads/filenames-test/src/main.rs
